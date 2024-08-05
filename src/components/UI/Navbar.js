3a7a01e5580cd32803eaf60f5c70f456958aadb4
raffyalbar30 <geminglol09@gmail.com>
2024-08-05 14:00:21 +0700

    "initial commit"

---
+++ src/components/UI/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="border w-full border-slate-500"> 
+            <div className="flex justify-between">
+               <span className="text-blue-700">Raffsume</span>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
