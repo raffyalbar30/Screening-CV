faaac33e4151169d72081016ee7477e10d41e330
raffyalbar30 <geminglol09@gmail.com>
2024-08-11 13:19:08 +0700

    "Responsive : navbar, and form uploader"

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -9,14 +9,14 @@ const font = Poppins({
 const Navbar = () => {
     return (
         <>
-         <div className={`${font.className} flex items-center ml-4`}>
+         <div className={`${font.className} flex items-center lg:ml-4`}>
          <div className="ml-8 px-4 py-2 rounded-lg bg-indigo-800">
              <span className="text-xl text-white">R</span>
          </div>
           <span className="ml-1 text-xl font-semibold text-slate-500 font-white">affsume</span>
          </div>
     
-        <div className="mr-16">
+        <div className="mr-16 hidden">
           <ul className={`${font.className} flex gap-x-6 font-semibold`}>
               <li>Buat Cv Sekarang</li>
               <li>Tempalate CV</li>
