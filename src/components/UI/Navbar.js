ea1a1fdabf8952b5bb1c8f2b3979d87867acadc4
raffyalbar30 <geminglol09@gmail.com>
2024-08-24 07:18:50 +0700

    "fixed responsive navbar"

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -20,8 +20,8 @@ const Navbar = () => {
           <span className="ml-1 text-xl font-semibold text-slate-500 font-white">affsume</span>
          </div>
     
-        <div className="mr-16 hidden lg:block md:block sm:block right-3">
-          <ul className={`${font.className} flex gap-x-6 font-semibold`}>
+        <div className=" hidden lg:block md:block sm:block">
+          <ul className={`${font.className} flex gap-x-6 absolute font-semibold right-8`}>
               <li>Buat Cv Sekarang</li>
               <li>Tempalate CV</li>
               <li>Feedback</li>
@@ -44,13 +44,13 @@ function Example() {
     <>
        <div className="fixed right-8 block lg:hidden md:hidden sm:block">
       <Menu>
-        <MenuButton className="inline-flex items-center gap-2 rounded-md bg-indigo-800 py-2 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-700 data-[open]:bg-indigo-400 data-[focus]:outline-1 data-[focus]:outline-white">
+        <MenuButton className="inline-flex items-center gap-2 rounded-md bg-indigo-800 py-2 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-indigo-400 data-[open]:bg-indigo-400 data-[focus]:outline-1 data-[focus]:outline-white">
         <RiMenu3Fill className="text-2xl" />
         </MenuButton>
         <MenuItems
           transition
           anchor="bottom end"
-          className="w-52 origin-top-right block lg:hidden md:hidden sm:block rounded-xl border border-slate-700 mt-2 bg-indigo-800 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
+          className="w-52 origin-top-right block lg:hidden md:hidden sm:block rounded-xl mt-2 bg-indigo-800 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
         >
           <MenuItem>
             <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
