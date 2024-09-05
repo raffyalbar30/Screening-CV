677acb67edaeae8521412498e4a34cf9607915a8
raffyalbar30 <geminglol09@gmail.com>
2024-09-05 20:47:09 +0700

    "still learn next js "

--- src/components/Uploader/uploader.js
+++ src/components/Uploader/uploader.js
@@ -15,47 +15,37 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 export default function Uploader() {
   const [getInputuser, setInputuser] = useState("");
   const [file, setfile] = useState(null);
+ 
+  
 
   const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_API_URL);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-  
-  const HandleSumbit =  async () => {
-    if (file != null) {
-       let reader = new FileReader();
-       const uploadResponse = reader.readAsDataURL(file);
-       reader.onload = function () {
-         console.log(reader.result);
-       };
-      
-      try {
 
-        await axios.post(`${genAI}`,uploadResponse, {
-          headers: {
-            'Content-Type': 'application/json',
-          }
-  
-        }).then(Ress => console.log(Ress))
-      //   const result = await model.generateContent([
-      //     {
-      //       fileData: {
-      //         mimeType: uploadResponse.file.mimeType,
-      //         fileUri: uploadResponse.file.uri
-      //       }
-      //     },
-      //     { text: getInputuser },
-      //   ]);
-      
-      // // Output the generated text to the console
-      // console.log(result.response.text())
-      
+  const HandleSumbit =  async () => {
+    const formData = new FormData(); 
+    formData.set("file", file);
+    console.log(file);
+    
+    
+     if (file != null) {
+      try {
+        const result = await model.generateContent([
+          {
+            fileData: {
+              mimeType: "application/pdf",
+              fileUri: "localhost:3000/pdf/CV.pdf"
+            }
+          },
+          { text: `Can you summarize this document as a bulleted list? ${getInputuser}` },
+        ]);
+
+        console.log(result.response.text());
+
       } catch (error) {
-        console.log(error);
-        
-      }
-    }
-    
-    
+        console.error("Error processing the file:", error);
+      } 
+    };
   }
 
   useEffect(() => {
@@ -66,14 +56,14 @@ export default function Uploader() {
     <>
      <div className="flex justify-center">
        <div className="mt-12">
-        <Input placeholder="your position apply..." setInputuser={setInputuser}/>
-         <FileUploader setfile={setfile}/>
-         <label htmlFor="" className='mt-4 lg:text-[12px] sm:text-[10px] md:text-[10px] text-[10px] ml-1 text-slate-600'>Anda memerlukan CV PDF untuk mengecek ATS-friendly</label>
-          <ReCAPTCHA sitekey={process.env.NEXT_PUBLIC_RECAPCATH_KEY } /> 
-           <Button HandleSumbit={HandleSumbit}>  
-            <CiSearch className="text-[24px]" />
-            Check ATS
-            </Button>
+          <Input placeholder="your position apply..." setInputuser={setInputuser}/>
+          <FileUploader setfile={setfile}/>
+          <label htmlFor="" className='mt-4 lg:text-[12px] sm:text-[10px] md:text-[10px] text-[10px] ml-1 text-slate-600'>Anda memerlukan CV PDF untuk mengecek ATS-friendly</label>
+            <ReCAPTCHA sitekey={process.env.NEXT_PUBLIC_RECAPCATH_KEY } /> 
+            <Button HandleSumbit={HandleSumbit}>  
+              <CiSearch className="text-[24px]" />
+              Check ATS
+              </Button>
           </div>
       </div>
     </>
